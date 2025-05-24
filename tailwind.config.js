5f6cbf28fb2982d6af6d198a808006848c10e19f
Mohamed <elkhamlichim099@gmail.com>
2025-05-24 15:22:54 +0100

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./**/*.{html,js}",
+        "./src/**/*.{html,js}",
+        "./user/**/*.{html,js}"
+    ],
+    plugins: [],
+}
